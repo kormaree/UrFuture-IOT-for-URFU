0326c7b4dc2c83370d00f75e23f05463e62bfe6f
Daniil Leshchev <leshev.da@gmail.com>
2025-05-30 15:50:47 +0500

    fix build errors

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from 'react';
+import { useContext } from 'react';
 import Panel from "../components/Panel";
 import { Link } from 'react-router-dom'
 import { AuthContext } from '../context/AuthContext';

d9bc29d7a00485dd9e32fec7581bd02aab1c68ba
Daniil Leshchev <leshev.da@gmail.com>
2025-06-14 22:35:56 +0500

    fetch recommendations for another profession function

--- src/api/recommendations.ts
+++ src/api/recommendations.ts
@@ -28,3 +28,12 @@ export async function fetchRecommendations(): Promise<SemesterRecommendations[]>
     const response = await api.get<SemesterRecommendations[]>('/recommendations/by-discipline/');
     return response.data;
 }
+
+export async function fetchRecommendationsForAnotherProfession(
+    id?: number
+): Promise<SemesterRecommendations[]> {
+    const params: Record<string, any> = { };
+    if (id) params.profession = id;
+    const response = await api.get<SemesterRecommendations[]>('/recommendations/by-discipline/', { params });
+    return response.data;
+}
